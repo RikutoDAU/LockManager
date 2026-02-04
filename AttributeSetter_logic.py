932fd03d1379e511881de030e4ec663095e871e5
Rikuto Tanaka <rikuto.12345st@gmail.com>
2026-02-04 17:41:14 +0900

    説明コメントの追加

--- AttributeSetter_logic.py
+++ AttributeSetter_logic.py
@@ -6,7 +6,8 @@ class attrSetter():
 
     def __init__(self):        
         pass
-
+    
+    #語尾の取得
     def getEndName(self, selection, endName):
 
         objList = []
@@ -17,8 +18,10 @@ class attrSetter():
 
         return objList                
 
+    #アトリビュートをロックする
     def attrLock(self, setObjList, lockAttrList, isInvisibility):
 
+        #指定されたオブジェクト1つ1つに対して、ロックするアトリビュートをそれぞれにロックや不可視化の操作
         for obj in setObjList:
 
             for i in lockAttrList:
@@ -34,6 +37,7 @@ class attrSetter():
                     except Exception as e:
                         print("アトリビュート見つからない" + {e})
 
+    #色の適用
     def applyColor(self, setObjList, colorRGBIndex):
 
         for obj in setObjList:
@@ -41,9 +45,11 @@ class attrSetter():
             
             if shapes:
 
+                #描画オーバーライドの項目をオンにする
                 for s in shapes:
                     cmds.setAttr(f"{s}.overrideEnabled", 1)
 
+                    #rgb値のリストの値を元に色を適用していく
                     if isinstance(colorRGBIndex, (list, tuple)):
                         cmds.setAttr(f"{s}.overrideRGBColors", 1)
 
@@ -53,33 +59,39 @@ class attrSetter():
                         cmds.setAttr(f"{s}.overrideColorRGB", r, g, b)
 
                     else:
-                        cmds.setAttr(f"{s}.overrideEnabled", 0)
+                        cmds.setAttr(f"{s}.overrideRGBColors", 0)
                         cmds.setAttr(f"{s}.overrideColor", colorRGBIndex)
 
             else:
                 pass
 
-
-    def doByGui(self, endName, doLockList, invisible, color):
+    #実行
+    def doByGui(self, endName, doLockList, invisible, changeColor, color):
 
         cmds.undoInfo(openChunk=True)
 
+        #選択中のオブジェクトを取得
         selection = cmds.ls(selection= True, long=True)
         if not selection:
             cmds.warning("何も選択されていません。")
             return
         
+        #語尾が一致するオブジェクトを取得してリスト化
         targetObjList = self.getEndName(selection, endName)
         if not targetObjList:
             cmds.warning("指定した語尾のオブジェクトがありません")
             return
         
+        #アトリビュートのロックと必要なら色の変更
         try:
             self.attrLock(targetObjList, doLockList, invisible)
 
-            self.applyColor(targetObjList, color)
+            if changeColor:
+
+                self.applyColor(targetObjList, color)
 
         except Exception as e:
+            cmds.undo()
             print(e)
 
         finally:
