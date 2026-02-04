8058e0ea0fc66dc10ca7b6c0548f7be31884a572
Rikuto Tanaka <rikuto.12345st@gmail.com>
2026-02-04 17:40:48 +0900

    説明コメント追加と軽い修正

--- AttributeSetter_Gui.py
+++ AttributeSetter_Gui.py
@@ -15,15 +15,11 @@ class attrSetGui(qw.QDialog):
         super().__init__(parent)
 
         self.logicClass = aS.attrSetter()
-        
-
 
         #ウィンドウの設定
         self.setWindowFlags(self.windowFlags() | qc.Qt.WindowStaysOnTopHint)
         self.setWindowTitle(self.windowName)
         self.resize(400,400)
-        #self.uiWindow()
-
         
         self.layout = qw.QVBoxLayout(self)
         self.layout.setSpacing(10)
@@ -35,43 +31,19 @@ class attrSetGui(qw.QDialog):
         self.explain = qw.QLabel(explainText)
         self.layout.addWidget(self.explain)
 
+        #指定する語尾の入力
         self.endNameInput = qw.QLineEdit()
         self.endNameInput.setPlaceholderText("操作したい語尾を入力してください。:例(_FK)")
         self.layout.addWidget(self.endNameInput)
 
-        '''
-        self.lockTrsXCb = qw.QCheckBox("X位置のロック")
-        self.lockTrsYCb = qw.QCheckBox("Y位置のロック")
-        self.lockTrsZCb = qw.QCheckBox("Z位置のロック")
-
-        self.lockRotXCb = qw.QCheckBox("X回転のロック")
-        self.lockRotYCb = qw.QCheckBox("Y回転のロック")
-        self.lockRotZCb = qw.QCheckBox("Z回転のロック")
-
-        self.lockTrsXCb.setChecked(True)
-        self.lockTrsYCb.setChecked(True)
-        self.lockTrsZCb.setChecked(True)
-
-        self.lockRotXCb.setChecked(True)
-        self.lockRotYCb.setChecked(True)
-        self.lockRotZCb.setChecked(True)
-'''
-        '''
-        self.lockTrsXCb.setProperty("attr", "tx")
-        self.lockTrsYCb.setProperty("attr", "ty")
-        self.lockTrsZCb.setProperty("attr", "tz")
-        self.lockRotXCb.setProperty("attr", "rx")
-        self.lockRotYCb.setProperty("attr", "ry")
-        self.lockRotZCb.setProperty("attr", "rz")
-        '''
-
+        #アトリビュートの
         self.lockCbList = []
         attrsList = [
             ("tx", "X位置"), ("ty", "Y位置"), ("tz", "Z位置"), 
             ("rx", "X回転"), ("ry", "Y回転"), ("rz", "Z回転"),
             ("sx", "Xスケール"), ("sy", "Yスケール"), ("sz", "Zスケール"),
         ]
-
+        #それぞれのアトリビュートに行う設定の表示
         for attr, attrName in attrsList:
             cb = qw.QCheckBox(attrName + "のロック")
             cb.setProperty("attr", attr)
@@ -79,76 +51,68 @@ class attrSetGui(qw.QDialog):
             self.layout.addWidget(cb)
             self.lockCbList.append(cb)
 
-        
+        #不可視にするかのチェックボックスを表示
         self.isInvisible = qw.QCheckBox("ロックした属性を不可視にする")
         self.isInvisible.setChecked(True)
         self.layout.addWidget(self.isInvisible)
 
-        #self.setColor = 
-
+        #色を変化させるかのチェックボックスを表示
+        self.changeColor = qw.QCheckBox("ワイヤーの色を変更する")
+        self.changeColor.setChecked(True)
+        self.layout.addWidget(self.changeColor)
+        
         self.layout.addWidget(qw.QLabel("ワイヤーの色を選択 (0-255)"))
 
+        #rgbの値の保存用リスト
         self.rgbIndexList = []
-
         rgbList = ["Red", "Green", "Blue"]
 
+        #r,g,b,それぞれのスライダーと数値入力の追加
         for rgb in rgbList:
             line = qw.QHBoxLayout()
             colorName = qw.QLabel(rgb)
+
+            #スライダー設定
             slider = qw.QSlider(qc.Qt.Horizontal)
             slider.setRange(0, 255) 
             slider.setValue(0)
 
+            #テキスト部分
             indexInput = qw.QLineEdit("0")
 
-
-
-
+            #スライダー操作時に更新
             slider.valueChanged.connect(lambda index, i=indexInput: i.setText(str(index)))
             slider.valueChanged.connect(self.updateColorPreview)
 
+            #テキスト入力後に更新
             indexInput.editingFinished.connect(lambda s=slider, i=indexInput: self.synchroSlider(s, i))
 
+            #ウィンドウに表示
             line.addWidget(colorName)
             line.addWidget(slider)
             line.addWidget(indexInput)
             self.layout.addLayout(line)
 
+            #rgbを保存
             self.rgbIndexList.append(slider)
 
+        #入力されているRGBによる色を表示
         self.colorPreview = qw.QWidget()
         self.colorPreview.setFixedHeight(30)
         self.layout.addWidget(self.colorPreview)
         self.updateColorPreview()
 
-
-
-        
-        '''
-        colorLayout = qw.QHBoxLayout()
-        
-        self.colorSlider = qw.QSlider(qc.Qt.Horizontal)
-        self.colorSlider.setRange(0, 31)
-        self.colorSlider.setValue(13)
-        
-        self.colorLabel = qw.QLabel("13")
-        
-        colorLayout.addWidget(self.colorSlider)
-        colorLayout.addWidget(self.colorLabel)
-        self.layout.addLayout(colorLayout)
-
-
-        self.colorSlider.valueChanged.connect(lambda v: self.colorLabel.setText(str(v)))
-        '''
+        #実行ボタンの表示
         self.doButton = qw.QPushButton("実行")
         self.doButton.clicked.connect(self.do)
         self.layout.addWidget(self.doButton)
 
+    #色設定のテキストとスライダーの数値を同期させる
     def synchroSlider(self, rgbSlider, rgbIndexText):
 
         text = rgbIndexText.text()
 
-        #数値のみか確認
+        #数値のみか確認と数値の修正
         if text.isdecimal():
             index = int(text)
 
@@ -157,6 +121,7 @@ class attrSetGui(qw.QDialog):
             elif index > 255:
                 index = 255
 
+            #スライダーとテキストに同じ値を入れる
             rgbSlider.setValue(index)
             rgbIndexText.setText(str(index))
 
@@ -164,22 +129,27 @@ class attrSetGui(qw.QDialog):
             rgbIndexText.setText(str(rgbSlider.value()))
 
         
-
+    #プレビュー用の色の更新
     def updateColorPreview(self):
+
+        #現在の指定されたrgb値を取得
         r = self.rgbIndexList[0].value()
         g = self.rgbIndexList[1].value()
         b = self.rgbIndexList[2].value()
 
+        #背景色としてrgb値を元に表示
         self.colorPreview.setStyleSheet(f"background-color:rgb({r},{g},{b});")
         
 
     def do(self):
-
+        
+        #指定された語尾取得
         endName = self.endNameInput.text()
 
         if not endName:
             cmds.error("語尾が入力されていない")
 
+        #ロックする属性をリスト化
         doLockList = []
 
         for cb in self.lockCbList:
@@ -188,28 +158,21 @@ class attrSetGui(qw.QDialog):
         
         invisible = self.isInvisible.isChecked()
 
+        changeColor = self.changeColor.isChecked()
+
+        #現在の指定されたrgb値を取得
         r = self.rgbIndexList[0].value()
         g = self.rgbIndexList[1].value()
         b = self.rgbIndexList[2].value()
         rgbIndexList = [r, g, b]
+    
 
-        #color = self.colorSlider.value()
 
-
-        self.logicClass.doByGui(endName, doLockList, invisible, rgbIndexList)
+        #実行
+        self.logicClass.doByGui(endName, doLockList, invisible, changeColor, rgbIndexList)
 
         self.close()
 
-        #self.logicClass.attrLock(objList, self.lockList, self.isInvisible)
-
-        #self.logicClass.applyColor(objList, )
-
-'''
-openWindow = attrSetGui()
-
-openWindow.setObjectName(openWindow.windowName)
-openWindow.show()
-'''
 openWindow = attrSetGui()
 
 openWindow.setObjectName(openWindow.windowName)
